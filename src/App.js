336c0f0ef787479e4ad2c161505178f8c82d4f67
avelarjr77 <oscaravelarje@gmail.com>
2021-04-15 23:06:53 -0600

    Acctualizando

--- src/App.js
+++ src/App.js
@@ -1,21 +1,27 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import Menu from './Paginas/Menu';
+import Submenu from './Paginas/Submenu';
+import Cliente from './Paginas/Jsx/Cliente';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          <Menu />
         </p>
+      </header>
+      <main>
+      <div>
+          <Submenu />
       </div>
-    );
-  }
+      <div>
+        <Cliente />
+      </div>
+        <p>@avelarjr77</p>
+      </main>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/Paginas/Jsx/Cliente/VerClientes1.js
@@ -0,0 +1,27 @@
+/* ejemplo de class based component
+siempre debe heredar de la clase Component
+*/
+
+import React, {Component} from 'react';
+import NuevoCliente1 from './NuevoCliente1';
+import NuevoCliente2 from './NuevoCliente2';
+import NuevoCliente3 from './NuevoCliente3';
+
+
+class verclientes1 extends Component{
+    render(){
+        return(
+            <div>
+                <p>Uso de class based components</p>
+                <NuevoCliente1 />
+                <NuevoCliente2 />
+                <NuevoCliente3 nombre='Luke SkyWalker' edad='25'/>
+
+            </div>
+        )
+    }
+
+
+}
+
+export default vercliente
